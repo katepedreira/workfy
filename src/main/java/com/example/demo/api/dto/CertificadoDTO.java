e1469e6525552fedc4835132b8422c6d0987d271
Katherine.Pedreira <kakamarpe@gmail.com>
2023-07-01 22:09:23 -0300

    creating other DTO classes

--- src/main/java/com/example/demo/api/dto/CertificadoDTO.java
+++ src/main/java/com/example/demo/api/dto/CertificadoDTO.java
@@ -1,4 +1,27 @@
 package com.example.demo.api.dto;
 
+import com.example.demo.model.entity.Certificado;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class CertificadoDTO {
+
+    private Long id;
+    private String titulo;
+    private Date dataEmissao;
+    private Date dataVencimento;
+
+    public static CertificadoDTO create(Certificado certificado) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(certificado, CertificadoDTO.class);
+    }
+
 }
--- src/main/java/com/example/demo/api/dto/EscalaDTO.java
+++ src/main/java/com/example/demo/api/dto/EscalaDTO.java
@@ -1,4 +1,27 @@
 package com.example.demo.api.dto;
 
+import com.example.demo.model.entity.Escala;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class EscalaDTO {
+    private Long id;
+    private String titulo;
+    private Date dataInicio;
+    private Date dataFim;
+    private Integer qtdDias;
+
+    public static EscalaDTO create(Escala escala) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(escala, EscalaDTO.class);
+    }
+
 }
